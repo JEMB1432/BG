7ba97573d80457bbcef316ea37e318c4b10a5553
Auri <20243ds056>
2025-06-30 20:39:11 -0600

    Se termina el modelo de la calse producto

--- src/main/java/jemb/bistrogurmand/views/Meseros/TomarpedidoView.java
+++ src/main/java/jemb/bistrogurmand/views/Meseros/TomarpedidoView.java
@@ -130,7 +130,19 @@ public class TomarpedidoView {
             modal.showAndWait();
         }
         public static class Producto{
-            
+            private String nombre;
+            private String categoria;
+            private double precio;
+
+            public Producto(String nombre, String categoria, double precio) {
+                this.nombre = nombre;
+                this.categoria = categoria;
+                this.precio = precio;
+            }
+
+            public String getNombre() { return nombre; }
+            public String getCategoria() { return categoria; }
+            public double getPrecio() { return precio; }
         }
 
 
