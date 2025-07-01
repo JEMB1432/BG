4ad2c711eecb73303838954ef1dddf215a172047
Auri <20243ds056>
2025-06-30 19:56:49 -0600

    Se agrego botones de categoria y los botones de cada categoria

--- src/main/java/jemb/bistrogurmand/views/Meseros/TomarpedidoView.java
+++ src/main/java/jemb/bistrogurmand/views/Meseros/TomarpedidoView.java
@@ -2,8 +2,12 @@ package jemb.bistrogurmand.views.Meseros;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class TomarpedidoView {
     private VBox content;
@@ -20,6 +24,17 @@ public class TomarpedidoView {
         VBox card = new VBox(15);
         card.setPadding(new Insets(20));
         card.setStyle("-fx-background-color: white; -fx-border-radius: 8px; -fx-background-radius: 8px; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.1), 10, 0, 0, 4);");
+
+        Label mesaLabel = new Label("Número de mesa:  #" + idMesa);
+        mesaLabel.setFont(Font.font("System", FontWeight.BOLD, 18));
+        //TomarPedidoView tomarPedido = new TomarPedidoView(mesaSeleccionada.getId());
+        //scene.setRoot(tomarPedido.getView());
+
+        Label categoriaLabel = new Label("Categoría:");
+        categoriaLabel.setFont(Font.font("System", FontWeight.BOLD, 14));
+
+        FlowPane categoriasBox = new FlowPane(10, 10); // Botones de categoría estilo píldora
+        categoriasBox.setPrefWrapLength(500);
 
 
     }
