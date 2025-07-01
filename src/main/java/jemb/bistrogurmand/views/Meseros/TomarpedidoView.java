14f84667256351f3de80ee9db240b5aace9bf931
Auri <20243ds056>
2025-06-30 19:22:17 -0600

    Se agrego tarjeta central blanca

--- src/main/java/jemb/bistrogurmand/views/Meseros/TomarpedidoView.java
+++ src/main/java/jemb/bistrogurmand/views/Meseros/TomarpedidoView.java
@@ -1,5 +1,30 @@
 package jemb.bistrogurmand.views.Meseros;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
+
 public class TomarpedidoView {
-    
+    private VBox content;
+    private ListView<String> resumenPedido;
+    private List<Producto> productos; // Lista simulada de productos disponibles
+
+    public TomarpedidoView(){
+        content = new VBox(20);
+        content.setPadding(new Insets(40));
+        content.setAlignment(Pos.TOP_LEFT);
+        content.setStyle("-fx-background-color: #f5f5f5");
+
+        //tarjeta central balnca
+        VBox card = new VBox(15);
+        card.setPadding(new Insets(20));
+        card.setStyle("-fx-background-color: white; -fx-border-radius: 8px; -fx-background-radius: 8px; -fx-effect: dropshadow(three-pass-box, rgba(0,0,0,0.1), 10, 0, 0, 4);");
+
+
+    }
+
+
+
+
 }
