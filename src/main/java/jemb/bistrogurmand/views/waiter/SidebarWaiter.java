0dee312775f6d1a0339954d31d1c61a155757542
FabianFuentes19 <20243ds032@utez.edu.mx>
2025-07-14 20:34:39 -0600

    problemas corregidos

--- src/main/java/jemb/bistrogurmand/views/waiter/SidebarWaiter.java
+++ src/main/java/jemb/bistrogurmand/views/waiter/SidebarWaiter.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.scene.shape.Circle;
@@ -16,6 +17,7 @@ import jemb.bistrogurmand.utils.User;
 import jemb.bistrogurmand.utils.UserSession;
 import jemb.bistrogurmand.application.App;
 
+import java.util.Arrays;
 import java.util.function.Consumer;
 
 
@@ -194,7 +196,7 @@ public class SidebarWaiter extends VBox {
     }
 
     private void setActiveButton(Button activeButton) {
-        Arrays.asList(btnJornada, btnTomarPedido, btnModificarPedido, btnMesasAsignadas)
+        Arrays.asList(btnDay, btnTakeOrder, btnModifyOrder, btnAssingnedTables)
                 .forEach(btn -> btn.getStyleClass().remove("active"));
         activeButton.getStyleClass().add("active");
     }
